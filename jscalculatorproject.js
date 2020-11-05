e439fc1ed0df228312f426af3d6464c20f984c5a
caitlyn <caitlynmiley.cm@gmail.com>
2020-11-04 21:06:43 -0600

    created a fully functioning calculator app

--- jscalculatorproject.js
+++ jscalculatorproject.js
@@ -1,7 +1,1 @@
-var cars = ["Test loop1", "test loop2", "test loop3", "Test loop4", "Test loop5", "Test loop6"];
-var text = "";
-var i;
-for (i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
-}
-document.getElementById("demo").innerHTML = text;
+
