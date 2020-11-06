a7e456ed5b8ea6f1a672326d1b341c701eaebc03
unknown <caitlynmiley.cm@gmail.com>
2020-11-06 13:16:13 -0600

    fixing chrome issue with inline js

--- calcapp/jscalculatorproject.js
+++ calcapp/jscalculatorproject.js
@@ -1,1 +1,9 @@
 
+
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+var text = "";
+var i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+document.getElementById("demo").innerHTML = text;
